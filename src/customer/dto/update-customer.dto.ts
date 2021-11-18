1fc7628f6eed0c0cabdde60c05c094b80fd126df
thien-lebk <59251858+thien-lebk@users.noreply.github.com>
2021-11-18 14:54:32 +0700

    #1811 add update customer dto swagger  property

--- src/customer/dto/update-customer.dto.ts
+++ src/customer/dto/update-customer.dto.ts
@@ -1,21 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
 
 export class UpdateCustomerDto {
-
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   fullName: string;
 
+  @ApiProperty()
   @IsString()
   note: string;
 
+  @ApiProperty()
   @IsPhoneNumber('VN')
   @IsNotEmpty()
   phoneNumber: string;
 
+  @ApiProperty()
   sendTime: Date;
 }
