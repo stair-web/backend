b222a17be7e137b79319bbc876184e16d367e1cb
Congkiet4695 <vophungcongkietiuh@gmail.com>
2021-12-21 18:35:35 +0700

    cron job update remain

--- src/lib/cron-job/cron-job.module.ts
+++ src/lib/cron-job/cron-job.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CronJobService } from './cron-job.service';
 import { CronJobController } from './cron-job.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserInformationRepository } from 'src/user-information/user-information.repository';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([UserInformationRepository]),
+  ],
   providers: [CronJobService],
   controllers: [CronJobController]
 })
